6c1c4c02f754f6e885867416e767268fa692ed1c
Petter Nyström <jimorie@gmail.com>
2017-07-02 22:29:29 +0200

    Tweaking setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 # -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 setup(
     name = 'thronescli',
     packages = ['thronescli'], # this must be the same as the name above
-    version = '0.2.3',
+    version = '0.2.4',
     description = 'A command line interface for looking up cards for A Game of Thrones LCG 2n Ed.',
     author = 'Petter Nyström',
     author_email = 'jimorie@gmail.com',
     url = 'https://github.com/jimorie/thronescli', # use the URL to the github repo
-    download_url = 'https://github.com/jimorie/thronescli/archive/0.2.3.tar.gz', # I'll explain this in a second
+    download_url = 'https://github.com/jimorie/thronescli/archive/0.2.4.tar.gz', # I'll explain this in a second
     keywords = ['game of thrones', 'thrones', 'thronesdb.com'], # arbitrary keywords
     classifiers = [],
     install_requires = [
@@ -21,7 +18,7 @@ setup(
     ],
     entry_points = {
         'console_scripts': [
-            'thronescli=thronescli:main'
+            'thronescli=thronescli.thronescli:main'
         ]
     }
 )
