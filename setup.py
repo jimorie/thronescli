2f73632f99bddab17a530c20356c82b42d37ce93
Petter Nyström <jimorie@gmail.com>
2017-07-02 18:17:28 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ except ImportError:
 setup(
     name = 'thronescli',
     packages = ['thronescli'], # this must be the same as the name above
-    version = '0.1',
+    version = '0.2.1',
     description = 'A command line interface for looking up cards for A Game of Thrones LCG 2n Ed.',
     author = 'Petter Nyström',
     author_email = 'jimorie@gmail.com',
     url = 'https://github.com/jimorie/thronescli', # use the URL to the github repo
-    download_url = 'https://github.com/jimorie/thronescli/archive/0.1.tar.gz', # I'll explain this in a second
+    download_url = 'https://github.com/jimorie/thronescli/archive/0.2.1.tar.gz', # I'll explain this in a second
     keywords = ['game of thrones', 'thrones', 'thronesdb.com'], # arbitrary keywords
     classifiers = [],
     install_requires = [
